f450f55a8a1ae553397e25bb68dcfb80dc3bfad0
Michael Sumner <mdsumner@gmail.com>
2016-04-04 14:52:56 +1000

    S3 for "sp_df"

--- inst/examples/exploring_data_frame_options.r
+++ inst/examples/exploring_data_frame_options.r
@@ -68,23 +68,58 @@ tSp <- function(x) {
   tab
 }
 
-x <- tSp(wrld_simpl)
+names.sp_df <- function(x, ...) {
+  x$Spatial <- NULL
+  names(x)
+}
 print.sp_df <- function(x, ...) {
-  x$Spatial <- sprintf("[%s %s]", class(x$Spatial), seq(nrow(x)))
-  class(x) <- setdiff(class(x), "sp_df")
-  print(x)
+  catclass <- class(x$Spatial)
+  x$Spatial <- NULL
+  
+  cat(catclass, "\n")
+  NextMethod("print", x)
 }
 
 as.data.frame.sp_df <- function(x, ...) {
   x$Spatial <- NULL
-  class(x) <- setdiff(class(x), "sp_df")
-  as.data.frame(x)
+  NextMethod("as.data.frame", x)
 }
+
 plot.sp_df <- function(x, ...) {
   px <- as.data.frame(x)
   plot(SpatialPolygonsDataFrame(x$Spatial, px, match.ID = FALSE), ...)
 }
 
+`[.sp_df` <- function(x, i, j, ...) {
+  id <- setNames(seq(nrow(x)), rownames(x))[i]
+  sp <- x[["Spatial"]]
+  #class(x) <- setdiff(class(x), "sp_df")
+  d <- NextMethod("[", x)
+  d$Spatial <- sp[id]
+  class(d) <- c("sp_df", class(d))
+  d
+}
+
+names.sp_df <- function(x) {
+  x$Spatial <- NULL
+  NextMethod("names", x)
+}
+dim.sp_df <- function(x) {
+  x$Spatial <- NULL
+  NextMethod("dim", x)
+}
+ncol.sp_df <- function(x) {
+  x$Spatial <- NULL
+  NextMethod("ncol", x)
+}
+as_Spatial.sp_df <- function(x, ...) {
+  df <- as.data.frame(x)
+  sp <- x$Spatial
+  ## etc. etc. 
+}
+## convert Spatial to a sp_df / tbl_df with the Spatial column
+x <- tSp(wrld_simpl)
+x %>% filter(NAME == "Australia")
 
 
 
