09e6ccc0fbadcefba7074c9540e7be4c022b2604
Michael Sumner <mdsumner@gmail.com>
2016-04-04 17:20:44 +1000

    remove sepNA

--- R/sptable.r
+++ R/sptable.r
@@ -125,24 +125,12 @@ geomnames <- function() {
 }
 
 ## adapted from raster package R/geom.R
-.polysGeom <-   function(x, sepNA=FALSE, ...) {
+.polysGeom <-   function(x,  ...) {
 
   nobs <- length(x@polygons)
   objlist <- list()
   cnt <- 0
-  if (sepNA) {
-    sep <- rep(NA,5)
-    for (i in 1:nobs) {
-      nsubobs <- length(x@polygons[[i]]@Polygons)
-      ps <- lapply(1:nsubobs,
-                   function(j)
-                     rbind(cbind(j, j+cnt, x@polygons[[i]]@Polygons[[j]]@hole, x@polygons[[i]]@Polygons[[j]]@coords), sep)
-      )
-      objlist[[i]] <- cbind(i, do.call(rbind, ps))
-      cnt <- cnt+nsubobs
-    }
-  } else {
-    for (i in 1:nobs) {
+  for (i in 1:nobs) {
       nsubobs <- length(x@polygons[[i]]@Polygons)
       ps <- lapply(1:nsubobs,
                    function(j)
@@ -151,52 +139,35 @@ geomnames <- function() {
       objlist[[i]] <- cbind(i, do.call(rbind, ps))
       cnt <- cnt+nsubobs
     }
-  }
+  
 
   obs <- do.call(rbind, objlist)
   colnames(obs) <- c('object', 'part', 'cump', 'hole', 'x', 'y')
   rownames(obs) <- NULL
 
-  if (sepNA) {
-    obs[is.na(obs[,2]), ] <- NA
-  }
+ 
   return( obs )
 }
 
 
 
 
-.linesGeom <-  function(x, sepNA=FALSE, ...) {
+.linesGeom <-  function(x,  ...) {
 
   nobs <- length(x@lines)
   objlist <- list()
   cnt <- 0
-  if (sepNA) {
-    sep <- rep(NA, 4)
-    for (i in 1:nobs) {
-      nsubobj <- length(x@lines[[i]]@Lines)
-      ps <- lapply(1:nsubobj,
-                   function(j)
-                     rbind(cbind(j, j+cnt, x@lines[[i]]@Lines[[j]]@coords), sep)
-      )
-      objlist[[i]] <- cbind(i, do.call(rbind, ps))
-      cnt <- cnt+nsubobj
-    }
-  } else {
-    for (i in 1:nobs) {
+for (i in 1:nobs) {
       nsubobj <- length(x@lines[[i]]@Lines)
       ps <- lapply(1:nsubobj, function(j) cbind(j, j+cnt, x@lines[[i]]@Lines[[j]]@coords))
       objlist[[i]] <- cbind(i, do.call(rbind, ps))
       cnt <- cnt+nsubobj
     }
-  }
+  
   obs <- do.call(rbind, objlist)
   colnames(obs) <- c('object', 'part', 'cump', 'x', 'y')
   rownames(obs) <- NULL
 
-  if (sepNA) {
-    obs[is.na(obs[,2]), ] <- NA
-  }
   return (obs)
 }
 
