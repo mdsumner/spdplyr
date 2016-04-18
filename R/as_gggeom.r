6c441765df3a1e2b65aae4a6d6005aeade452491
Michael Sumner <mdsumner@gmail.com>
2016-04-18 23:18:50 +1000

    gggeom

---
+++ R/as_gggeom.r
@@ -0,0 +1,29 @@
+cna <- function(x, na = NA_real_) {
+  c(x, na)
+}
+
+#' Title
+#'
+#' @param x 
+#' @param ... 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+as_gggeom <- function(x,  ...) {
+  data <- as.data.frame(x)
+  x <- sptable(x)
+  data$y_ <- data$x_ <- vector("list", nrow(data))
+  for (i in unique(x$object)) {
+    asub <- x[x$object == i, c("branch", "x", "y")]
+    data$x_[[i]] <- unname(head(unlist(lapply(split(asub$x, asub$branch), cna)), -1))
+    data$y_[[i]] <- unname(head(unlist(lapply(split(asub$y, asub$branch), cna)), -1))
+  } 
+  class(data$x_) <- "coords"
+  class(data$y_) <- "coords"
+  
+  class(data) <- c("geom_polygon", "geom_path", "geom", "data.frame")
+  data
+}
+
