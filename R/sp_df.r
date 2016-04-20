62b96e8b6e5313bce2fa1e43decfe615efb83f74
Michael Sumner <Michael.Sumner@aad.gov.au>
2016-04-20 15:30:48 +1000

    simplification of sp_df

--- R/sp_df.r
+++ R/sp_df.r
@@ -20,26 +20,13 @@ has_data_frame <- function(x) {
   }
 }
 
-# explode_ <- function(x) {
-#   l <- vector('list', length(x))
-#   for (i in seq_along(l)) l[[i]] <- x[i]
-#   l
-# }
+
 #' @export
 sp_df.Spatial <- function(x, ...) {
   tab <- has_data_frame(x)
   tab$Spatial_ <- geometry(x)
   class(tab) <- c("sp_df", class(tab))
   tab
-}
-
-#' @export
-`[.sp_df` <- function(x, i, j, ...) {
-  ## this cannot work if i has duplicates (can't find plot order if polygons are replicated)
-  ## though it does work for Spatial*DataFrame
-  d <- NextMethod(x)
-  class(d) <- c("sp_df", class(d))
-  d
 }
 
 #' @export
@@ -54,47 +41,3 @@ obj_str.Lines <- function(x) sprintf("%s[%i]", class(x), length(x@Lines))
 obj_str.Polygons <- function(x) sprintf("%s[%i]", class(x), length(x@Polygons))
 
 
-
-# #' @export
-# #' @rdname sp_df
-# #' @rawNamespace S3method(plot,sp_df)
-# plot.sp_df <- function(x, ...) {
-#   px <- as.data.frame(x)
-#   plot(spFromTable(x[["Spatial_"]], as.data.frame(x)), ...)
-# }
-
-as.data.frame.sp_df <- function(x, ...) {
-  x$Spatial <- NULL
-  NextMethod("as.data.frame", x)
-}
-
-
-
-
-names.sp_df <- function(x) {
-  x$Spatial <- NULL
-  NextMethod("names", x)
-}
-dim.sp_df <- function(x) {
-  x$Spatial <- NULL
-  NextMethod("dim", x)
-}
-ncol.sp_df <- function(x) {
-  x$Spatial <- NULL
-  NextMethod("ncol", x)
-}
-as_Spatial.sp_df <- function(x, ...) {
-  df <- as.data.frame(x)
-  sp <- x[["Spatial"]]
-  .detectSpatial(class(sp))(sp, df, proj4string = CRS(attr(x, "crs")), match.ID = FALSE)
-   
-}
-
-#' @importFrom sp SpatialMultiPointsDataFrame
-.detectSpatial <- function(x) {
-  switch(x, 
-         SpatialPoints = SpatialPointsDataFrame,
-         SpatialMultipoints = SpatialMultiPointsDataFrame,
-         SpatialLines = SpatialLinesDataFrame, 
-         SpatialPolygons = SpatialPolygonsDataFrame)
-}
