1ce671eecef30f69f8521afdffca0e7f9404400a
Michael Sumner <mdsumner@gmail.com>
2016-07-30 08:41:19 +1000

    spelling, and single quotes for software

--- R/utils.r
+++ R/utils.r
@@ -54,3 +54,10 @@ setMethod ('show' , 'SpatialPoints',
              .print_Spatial(object)
            }
 )
+
+
+release_questions <- 
+  c("Have you single-quoted package and other non-English names in the DESCRIPTION and TITLE?\nhttps://cran.r-project.org/doc/manuals/r-release/R-exts.html#The-DESCRIPTION-file", 
+  c("Have you manually inspected your rendered vignettes?"))
+    
+    
