8bb4428a52d3ee433628bf598bb5fdaef91f7a18
Michael Sumner <mdsumner@gmail.com>
2017-06-03 01:33:59 +1000

    turn off badge for CRAN

---
+++ R/release-questions.R
@@ -0,0 +1,3 @@
+release_questions <- function()
+  c("Have you single-quoted package and other non-English names in the DESCRIPTION and TITLE?\nhttps://cran.r-project.org/doc/manuals/r-release/R-exts.html#The-DESCRIPTION-file", 
+    "Have you manually inspected your rendered vignettes?")
